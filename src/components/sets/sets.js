6f38051810b37d4ac147d865f6b66dc738be8009
vlad3k <vlad3k@bk.ru>
2019-10-22 18:01:03 +0500

    initially styles for sets

--- src/components/sets/sets.js
+++ src/components/sets/sets.js
@@ -2,7 +2,97 @@ import React from "react";
 import "./sets.css";
 
 const Sets = () => {
-  return <div>sets</div>;
+  return (
+    <div className="sets">
+      <a className="set">
+        <div>
+          <div className="set-image">
+            <img src="https://images.pokemontcg.io/sma/logo.png" alt="" />
+          </div>
+          <div className="set-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image">
+                  <img
+                    src="https://images.pokemontcg.io/sma/symbol.png"
+                    alt=""
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title">Shiny Vault</p>
+                <p className="subtitle">Released 08/23/2019</p>
+              </div>
+            </div>
+            <div className="content">
+              <ul>
+                <li>Standard Legal</li>
+                <li>Expanded Legal</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a className="set">
+        <div>
+          <div className="set-image">
+            <img src="https://images.pokemontcg.io/sma/logo.png" alt="" />
+          </div>
+          <div className="set-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image">
+                  <img
+                    src="https://images.pokemontcg.io/sma/symbol.png"
+                    alt=""
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title">Shiny Vault</p>
+                <p className="subtitle">Released 08/23/2019</p>
+              </div>
+            </div>
+            <div className="content">
+              <ul>
+                <li>Standard Legal</li>
+                <li>Expanded Legal</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </a>
+      <a className="set">
+        <div>
+          <div className="set-image">
+            <img src="https://images.pokemontcg.io/sma/logo.png" alt="" />
+          </div>
+          <div className="set-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image">
+                  <img
+                    src="https://images.pokemontcg.io/sma/symbol.png"
+                    alt=""
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title">Shiny Vault</p>
+                <p className="subtitle">Released 08/23/2019</p>
+              </div>
+            </div>
+            <div className="content">
+              <ul>
+                <li>Standard Legal</li>
+                <li>Expanded Legal</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
 };
 
 export default Sets;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import Sets from "./components/sets";
+
 import "./index.css";
 
-ReactDOM.render(<h1>Hello!</h1>, document.getElementById("root"));
+ReactDOM.render(<Sets />, document.getElementById("root"));
